a124e137173dec4b94a75413f8af5880e401cded
Gerardo Lucero <gera_conecta@hotmail.com>
2019-12-18 15:48:24 -0700

    se modifico el serve.js

--- serve.js
+++ serve.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const port = process.env.PORT || 8080
-const app = express()
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/dist'));
 app.get(/.*/, function(req, res) {
-    res.sendfile(__dirname + '/dis/index.html')
+    res.sendfile(__dirname + '/dis/index.html');
 })
-app.listen(port)
-console.log('Server started...')
+app.listen(port);
+console.log('Server started...');
