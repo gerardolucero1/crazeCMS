c14d4a22d4dc2c1f544657a040b99b4daa9d84cf
Gerardo Lucero <gera_conecta@hotmail.com>
2019-12-18 15:19:21 -0700

    build to heroku

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,22 @@ require('firebase/functions')
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCqauZ8ks03Cjwrx6nR_oldJMXtKylASvg",
-    authDomain: "craze-85d97.firebaseapp.com",
-    databaseURL: "https://craze-85d97.firebaseio.com",
-    projectId: "craze-85d97",
-    storageBucket: "craze-85d97.appspot.com",
-    messagingSenderId: "399502940312",
-    appId: "1:399502940312:web:8655f232b9db204a8a18ed",
-    measurementId: "G-V0ZLYP6D0N"
+    // apiKey: "AIzaSyCqauZ8ks03Cjwrx6nR_oldJMXtKylASvg",
+    // authDomain: "craze-85d97.firebaseapp.com",
+    // databaseURL: "https://craze-85d97.firebaseio.com",
+    // projectId: "craze-85d97",
+    // storageBucket: "craze-85d97.appspot.com",
+    // messagingSenderId: "399502940312",
+    // appId: "1:399502940312:web:8655f232b9db204a8a18ed",
+    // measurementId: "G-V0ZLYP6D0N"
+    apiKey: "AIzaSyB-Y8DHYC5ki4EKBojAfalAGYFPCuHaAFU",
+    authDomain: "craze-71d6b.firebaseapp.com",
+    databaseURL: "https://craze-71d6b.firebaseio.com",
+    projectId: "craze-71d6b",
+    storageBucket: "craze-71d6b.appspot.com",
+    messagingSenderId: "999017124538",
+    appId: "1:999017124538:web:524a9bb2cad04380d72883",
+    measurementId: "G-TCL8FH6GTW"
 };
 
 firebase.initializeApp(firebaseConfig)
--- src/views/prenda/Prendas.vue
+++ src/views/prenda/Prendas.vue
@@ -7,17 +7,18 @@
         <v-container grid-list-xs>
             <v-row>
                 <v-col>
+                    <v-img
+                        class="white--text align-end"
+                        width="100%"
+                        :src="prenda.foto"
+                        >
+                        </v-img>
+                </v-col>
+                <v-col>
                    <v-card v-if="prenda.length != 0"
                         class="mx-auto"
                         max-width="400"
                     >
-                        <v-img
-                        class="white--text align-end"
-                        height="200px"
-                        :src="prenda.foto"
-                        >
-                        </v-img>
-
                         <v-card-title>{{ prenda.nombre }}</v-card-title>
                         <v-divider></v-divider>
                         <v-chip
@@ -33,18 +34,78 @@
                         </v-card-text>
 
                         <v-card-actions>
-                        <v-btn
-                            color="orange"
-                            text
-                        >
-                            Share
-                        </v-btn>
+                        <template>
+                            <v-dialog v-model="editarPrenda" persistent max-width="600px">
+                                <template v-slot:activator="{ on }">
+                                    <v-btn color="primary" text v-on="on" @click="prendaEdicion = JSON.parse( JSON.stringify( prenda ) )">Editar</v-btn>
+                                </template>
+                                <v-card>
+                                    <v-card-title class="headline grey lighten-2" primary-title>
+                                        <span class="headline">Prenda</span>
+                                    </v-card-title>
+
+                                    <v-card-text>
+                                        <v-container>
+                                            <v-row>
+                                                <v-col cols="12" md="12">
+                                                    <v-text-field label="Nombre" required v-model="prendaEdicion.nombre"></v-text-field>
+                                                </v-col>
+                                                <v-col cols="12" md="12">
+                                                    <v-text-field label="Marca" required v-model="prendaEdicion.marca"></v-text-field>
+                                                </v-col>
+                                                <v-col>
+                                                    <v-select
+                                                    :items="categorias"
+                                                    item-text="nombre"
+                                                    item-value="id"
+                                                    label="Categoria"
+                                                    v-model="categoriaSeleccionada"
+                                                    ></v-select>
+                                                </v-col>
+                                                <v-col v-if="categoriaSeleccionada != null">
+                                                    <v-select
+                                                        v-model="prendaEdicion.tallas"
+                                                        :items="categoria.tallas"
+                                                        label="Tallas"
+                                                        multiple
+                                                        chips
+                                                    ></v-select>
+                                                </v-col>
+                                                <v-col cols="12" md="12">
+                                                    <v-textarea label="Descripcion" required v-model="prendaEdicion.descripcion"></v-textarea>
+                                                </v-col>
+                                                <v-col cols="12" md="12">
+                                                    <file-pond v-if="vista == 1"
+                                                        label-idle="Selecciona una imagen" 
+                                                        accepted-file-types="image/jpeg, image/png, image/jpg"
+                                                        labelFileTypeNotAllowed="SOlo imagenes jpeg o png"
+                                                        instant-upload="false"
+                                                        @addfile="cargarImagen"
+                                                        >
+                                                    </file-pond>
+                                                    <img v-if="vista == 2" ref="vistaPrevia" src="" alt="" width="100%">
+                                                </v-col>
+                                            </v-row>
+                                        </v-container>
+                                    </v-card-text>
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn color="primary" text @click="editarPrenda = false, prendaEdicion = ''">
+                                            Cancel
+                                        </v-btn>
+                                        <v-btn color="primary" text @click="guardarPrenda">
+                                            Guardar
+                                        </v-btn>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
+                        </template>
 
                         <v-btn
-                            color="orange"
+                            color="red"
                             text
                         >
-                            Explore
+                            Eliminar
                         </v-btn>
                         </v-card-actions>
                     </v-card>              
@@ -55,29 +116,135 @@
 </template>
 
 <script>
-import { db, storage } from '@/firebase'
 import { mapState } from 'vuex'
+import { db, storage } from '@/firebase'
+
+import vueFilePond from 'vue-filepond'
+import FilePondPluginValidateType from 'filepond-plugin-file-validate-type'
+import 'filepond/dist/filepond.min.css'
+
+import Cropper from 'cropperjs/dist/cropper'
+import 'cropperjs/dist/cropper.css'
+
+import uuidv4 from 'uuid/v4'
+
+const FilePond = vueFilePond(FilePondPluginValidateType)
 
 export default {
     data(){
         return{
             prenda: '',
+            editarPrenda: false,
+            prendaEdicion: '',
+
+            vista: 1,
+
+            categoriaSeleccionada: null,
+            categoria: '',
+            categorias: [],
+
+            tallas: [],
+
+            boutique: '',
         }
     },
     async mounted() {
         let id = this.$route.params.id
-        try {
-            let response = await db.collection('prendas').doc(this.sesion.usuario.boutique).collection('ropa').doc(id).get()
-            if(response.exists){
-                this.prenda = response.data()
-            }
-        } catch (e) {
-            console.log(e)
-        }
+
+        await db.collection('prendas').doc(this.sesion.usuario.boutique).collection('ropa').doc(id).onSnapshot(response => {
+            this.prenda = response.data()
+            this.prendaEdicion = JSON.parse( JSON.stringify( this.prenda ) )
+
+        })
+
+        await db.collection('boutiques').doc(this.sesion.usuario.boutique).onSnapshot(response => {
+            this.boutique = response.data()
+        })
+
+        await db.collection('categorias').onSnapshot(response => {
+            this.categorias = [];
+            response.forEach(doc => {
+                this.categorias.push(doc.data())
+            })
+        })
         
     },
     computed: {
         ...mapState(['sesion'])
+    },
+    watch:{
+        categoriaSeleccionada(){
+            this.categoria = this.categorias.find(element => element.id == this.categoriaSeleccionada)
+        }
+    },
+    methods: {
+        async guardarPrenda(){
+            let idPrenda = this.$route.params.id
+            try {
+                let response = await db.collection('prendas').doc(this.sesion.usuario.boutique).collection('ropa').doc(idPrenda).set(this.prendaEdicion, {merge: true})
+                
+                if(this.cropper){
+                    let canvas = this.cropper.getCroppedCanvas()
+                    let imagen = canvas.toDataURL('image/png')
+                    let fotoId = uuidv4()
+
+                    try {
+                        let ref = storage.ref()
+                        let resultado = await ref.child('boutiques/' + this.boutique.nombre + '/prendas/' + fotoId + '.png')
+                                                .putString(imagen, 'data_url')
+                        
+                        let URL = await resultado.ref.getDownloadURL()
+
+                        await db.collection('prendas').doc(this.sesion.usuario.boutique).collection('ropa').doc(idPrenda).update({ foto: URL })
+                    } catch (e) {
+                        console.log(e)
+                    }
+                }
+                
+                this.prendaEdicion = ''
+                this.editarPrenda = false
+            } catch (e) {
+                console.log(e)
+            }
+        },
+
+        cargarImagen(error, imagen){
+            if(error){
+                console.log(error)
+                return
+            }
+            console.log(imagen.file)
+            this.vista = 2
+
+            let reader = new FileReader()
+
+            let vm = this
+
+            reader.onloadend= () => {
+                vm.$refs.vistaPrevia.src = reader.result
+                vm.recortarImagen()
+            }
+
+            if(imagen){
+                if(imagen.file){
+                    reader.readAsDataURL(imagen.file)
+                }
+            }
+        },
+
+        recortarImagen(){
+            this.cropper = new Cropper(this.$refs.vistaPrevia, {
+                preview: this.$refs.vistaPrevia,
+                //aspectRatio: 1,
+                modal: false,
+                guides: true,
+                sized: false,
+                zoomable: false,
+                highlight: true,
+                background: false,
+                autoCrop: true,
+            })
+        },
     },
 }
 </script>
