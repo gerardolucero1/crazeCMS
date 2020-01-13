ed07c577d83120521dfa43ae39e387306bdfd2dc
havillalobos331 <horacio@partnergrammer.com>
2020-01-13 13:11:41 -0700

    nuevo dashboard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,116 @@
+<style lang="">
+
+</style>
+
 <template>
-    <v-layout wrap>
-        <v-flex lg3>
-            <div>
-            <p>Likes totales: {{totalLikes}}</p>
-            <v-data-table
-                :headers="headers"
-                :items="listaLikes"
-                :items-per-page="5"
-                class="elevation-1"
-                loading="true"
+    <v-layout>
+        <v-container grid-list-xs>
+            <v-row>
+                <v-col>
+                    <v-card
+                    class="mx-auto"
+                    max-width="400"
+                    >
+                        <v-card-title>Total de prendas subidas</v-card-title>
 
-            >
-            </v-data-table>
-            </div>
-            <div>
-            <p>Dislikes totales: {{totalDislikes}}</p>
-            <v-data-table
-                :headers="headers2"
-                :items="listaDislikes"
-                :items-per-page="5"
-                class="elevation-1"
-                loading="true"
+                        <v-card-text class="headline text-center font-weight-bold">
+                          3
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-card
+                    class="mx-auto"
+                    max-width="400"
+                    >
+                        <v-card-title>Likes esta semana</v-card-title>
 
-            >
-            </v-data-table>
-            </div>
-        </v-flex>
+                        <v-card-text class="headline text-center font-weight-bold">
+                          13
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col>
+                    <v-card
+                    class="mx-auto"
+                    max-width="400"
+                    >
+                        <v-card-title>Dislikes esta semana</v-card-title>
+
+                        <v-card-text class="headline text-center font-weight-bold">
+                          17
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-card
+                    class="mx-auto"
+                    max-width="400"
+                    >
+                        <v-img
+                          class="white--text align-end"
+                          height="200px"
+                          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                        >
+                          <v-card-title>Prenda con mas likes esta semana</v-card-title>
+                        </v-img>
+
+                        <v-card-text class="headline text-center font-weight-bold">
+                          TOPSHOP SWEATER
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col>
+                    <v-card
+                    class="mx-auto"
+                    max-width="400"
+                    >
+                        <v-img
+                          class="white--text align-end"
+                          height="200px"
+                          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                        >
+                          <v-card-title>Prenda con mas dislikes esta semana</v-card-title>
+                        </v-img>
+
+                        <v-card-text class="headline text-center font-weight-bold">
+                          Express sequin slip dress
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-card
+                    class="mx-auto"
+                    max-width="400"
+                    >
+                        <v-card-title class="justify-center">% de cambio en likes</v-card-title>
+                        <v-card-title class="justify-center">respecto a la semana pasada</v-card-title>
+                        <v-card-text class="headline text-center font-weight-bold">
+                          56%
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col>
+                    <v-card
+                    class="mx-auto"
+                    max-width="400"
+                    >
+                        <v-card-title class="justify-center">% de cambio en dislikes</v-card-title>
+                        <v-card-title class="justify-center">respecto a la semana pasada</v-card-title>
+                        <v-card-text class="headline text-center font-weight-bold">
+                          56%
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-layout>
-
 </template>
 
 <script>
@@ -41,8 +124,8 @@ export default {
             publicadas : [],
             listaLikes : [],
             listaDislikes : [],
-            totalLikes : 0,
-            totalDislikes : 0,
+            totalLikes : 13,
+            totalDislikes : 17,
 
 
             headers: [
@@ -76,33 +159,35 @@ export default {
         ...mapState(['sesion'])
     },
     async mounted() {
-        let boutiqueId = 'bzTPm28OeUu9NghDhJln'//this.sesion.user.boutique
-        this.listaLikes = []
-        this.listaDislikes = []
-        this.totalLikes = 0
-        this.totalDislikes = 0
-
-        await db.collection('prendas').where('boutique','==',boutiqueId).get().then( snapshot =>{
-            snapshot.forEach(doc=>{
-                this.prendas.push(doc.data())
-                if(doc.data().publicado){
-                    this.publicadas.push(doc.data())
-                }
-
-                db.collectionGroup('like').where('id','==',doc.data().id).get().then( snapshot=>{
-                    this.listaLikes.push({name: doc.data().name, likes: snapshot.size})
-                })
-                db.collectionGroup('dislike').where('id','==',doc.data().id).get().then( snapshot=>{
-                    this.listaDislikes.push({name: doc.data().name, dislikes: snapshot.size})
-                })
-            })
-        })
-        for (let i = 0; i<this.listaLikes.length; i++){
-          this.totalLikes+=this.listaLikes[i].likes;
-        }
-        for (let i = 0; i<this.listaDislikes.length; i++){
-          this.totalDislikes+=this.listaDislikes[i].dislikes;
-        }
+    this.prendas = [{name: Sueter, likes: 3, dislikes: 2},{name: Pantalon, likes: 10, dislikes: 5}, {name: Camisa, likes: 0, dislikes: 10}];
+    this.publicadas = [{name: Sueter, likes: 3, dislikes: 2},{name: Pantalon, likes: 10, dislikes: 5}];
+//        let boutiqueId = 'bzTPm28OeUu9NghDhJln'//this.sesion.user.boutique
+//        this.listaLikes = []
+//        this.listaDislikes = []
+//        this.totalLikes = 0
+//        this.totalDislikes = 0
+//
+//        await db.collection('prendas').where('boutique','==',boutiqueId).get().then( snapshot =>{
+//            snapshot.forEach(doc=>{
+//                this.prendas.push(doc.data())
+//                if(doc.data().publicado){
+//                    this.publicadas.push(doc.data())
+//                }
+//
+//                db.collectionGroup('like').where('id','==',doc.data().id).get().then( snapshot=>{
+//                    this.listaLikes.push({name: doc.data().name, likes: snapshot.size})
+//                })
+//                db.collectionGroup('dislike').where('id','==',doc.data().id).get().then( snapshot=>{
+//                    this.listaDislikes.push({name: doc.data().name, dislikes: snapshot.size})
+//                })
+//            })
+//        })
+//        for (let i = 0; i<this.listaLikes.length; i++){
+//          this.totalLikes+=this.listaLikes[i].likes;
+//        }
+//        for (let i = 0; i<this.listaDislikes.length; i++){
+//          this.totalDislikes+=this.listaDislikes[i].dislikes;
+//        }
     }
 }
 </script>
