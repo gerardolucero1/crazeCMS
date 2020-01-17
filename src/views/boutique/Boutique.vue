2bd1bbb347d4a7b8726cd9a3953d15651afa4e78
havillalobos331 <horacio@partnergrammer.com>
2020-01-17 13:22:24 -0700

    mover funcionaligad de /register a /Boutique

--- src/views/boutique/Boutique.vue
+++ src/views/boutique/Boutique.vue
@@ -50,6 +50,77 @@
                                 </v-card-actions>
                             </v-card>
                         </v-dialog>
+                        <v-dialog v-model="modalRegistro" persistent max-width="600px">
+                            <template v-slot:activator="{ on }">
+                                <v-btn color="primary" dark v-on="on">Registrar Usuario</v-btn>
+                            </template>
+                            <v-card>
+                            <v-card-text>
+                                <v-text-field
+                                    :error-messages="erroresEmail"
+                                    @blur="$v.formulario.email.$touch()"
+                                    name="email"
+                                    label="email"
+                                    id="email"
+                                    v-model="formulario.email"
+                                ></v-text-field>
+
+                                <v-text-field
+                                    :error-messages="erroresPassword"
+                                    @blur="$v.formulario.password.$touch()"
+                                    name="password"
+                                    label="password"
+                                    id="password"
+                                    :append-icon="showNormal ? 'visibility' : 'visibility_off'"
+                                    @click:append="() => (showNormal = !showNormal)"
+                                    :type="showNormal ? 'password' : 'text'"
+                                    v-model="formulario.password"
+                                ></v-text-field>
+
+                                <v-text-field
+                                    :error-messages="erroresRepetirPassword"
+                                    @blur="$v.formulario.repetirPassword.$touch()"
+                                    name="password"
+                                    label="Repetir password"
+                                    id="password"
+                                    :append-icon="showRepeat ? 'visibility' : 'visibility_off'"
+                                    @click:append="() => (showRepeat = !showRepeat)"
+                                    :type="showRepeat ? 'password' : 'text'"
+                                    v-model="formulario.repetirPassword"
+
+                                ></v-text-field>
+                                <v-text-field
+                                    :error-messages="erroresNombres"
+                                    @blur="$v.formulario.nombres.$touch()"
+                                    autofocus
+                                    name="name"
+                                    label="name"
+                                    id="name"
+                                    v-model="formulario.nombres"
+                                ></v-text-field>
+
+                                <v-text-field
+                                    :error-messages="erroresApellidos"
+                                    @blur="$v.formulario.apellidos.$touch()"
+                                    name="apellido"
+                                    label="apellido"
+                                    id="apellido"
+
+                                    v-model="formulario.apellidos"
+                                ></v-text-field>
+                            </v-card-text>
+
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn color="primary" text @click="modalRegistro = false">
+                                    Cancel
+                                </v-btn>
+                                <v-btn :disabled="$v.formulario.$invalid" color="primary" text @click="registrarEmail">
+                                    Guardar
+                                </v-btn>
+                            </v-card-actions>
+                            </v-card>
+                        </v-dialog>
                         <v-card class="elevation-2 ma-3">
                             <v-img :src="boutique.logo" min-height= "300px" max-height= "600px">
 
@@ -215,7 +286,7 @@
 
 <script>
 import { mapState } from 'vuex'
-import { db, storage } from '@/firebase'
+import { auth, firebase, db , storage } from '@/firebase'
 
 import vueFilePond from 'vue-filepond'
 import FilePondPluginValidateType from 'filepond-plugin-file-validate-type'
@@ -227,6 +298,10 @@ import 'cropperjs/dist/cropper.css'
 import uuidv4 from 'uuid/v4'
 
 import Swal from 'sweetalert2'
+
+import { required, email, minLength, maxLength, sameAs } from 'vuelidate/lib/validators'
+import { nombreCompuesto } from '@/utilities/validations'
+import { mapMutations, mapGetters } from 'vuex'
 
 const FilePond = vueFilePond(FilePondPluginValidateType)
 
@@ -275,6 +350,57 @@ export default {
                 foto: '',
             },
             tallas: [],
+
+            //Registro usuario
+            modalRegistro: false,
+
+            formulario: {
+                email: '',
+                password: '',
+                repetirPassword: '',
+                nombres: '',
+                apellidos: '',
+            },
+
+            metodo: 'password',
+            showNormal: String,
+            showRepeat: String
+        }
+    },
+    validations: {
+        formulario: {
+            email: {
+                required,
+                email,
+            },
+            password: {
+                required,
+                minLength: minLength(6),
+                maxLength: maxLength(20),
+            },
+            repetirPassword: {
+                sameAs: sameAs('password'),
+                required,
+            },
+            nombres: {
+                required,
+                minLength: minLength(3),
+                maxLength: maxLength(20),
+                nombreCompuesto
+            },
+            apellidos: {
+                required,
+                minLength: minLength(3),
+                maxLength: maxLength(20),
+                nombreCompuesto
+            },
+        },
+    },
+    created(){
+        if(auth.currentUser && !this.$store.state.sesion.usuario){
+            this.metodo = auth.currentUser.providerData[0].providerId
+            this.vista = 2
+            this.$store.commit('mostrarInformacion', 'Completa tus datos de registro')
         }
     },
     computed: {
@@ -286,7 +412,115 @@ export default {
             return this.prendas.filter((i) => {
               return !this.categoria || (i.categoria === this.categoria);
             })
-        }
+        },
+        ...mapGetters(
+            'sesion', ['saludo']
+        ),
+        //Errores registro
+        erroresEmail: function(){
+            let errores = []
+            if(!this.$v.formulario.email.$dirty){
+                return errores
+            }
+
+            if(!this.$v.formulario.email.required){
+                errores.push('Ingresa tu email')
+            }
+
+            if(!this.$v.formulario.email.email){
+                errores.push('Ingresa un email valido')
+            }
+
+            return errores
+        },
+
+        erroresPassword: function(){
+            let errores = []
+            if(!this.$v.formulario.password.$dirty){
+                return errores
+            }
+
+            if(!this.$v.formulario.password.required){
+                errores.push('Ingresa tu password')
+            }
+
+            if(!this.$v.formulario.password.minLength){
+                errores.push('Ingresa un password de minimo 6 caracteres')
+            }
+
+            if(!this.$v.formulario.password.maxLength){
+                errores.push('Ingresa un password de maximo 20 caracteres')
+            }
+
+            return errores
+        },
+
+        erroresRepetirPassword: function(){
+            let errores = []
+            if(!this.$v.formulario.repetirPassword.$dirty){
+                return errores
+            }
+
+            if(!this.$v.formulario.repetirPassword.required){
+                errores.push('Ingresa tu password')
+            }
+
+            if(!this.$v.formulario.repetirPassword.sameAs){
+                errores.push('Las contraseñas no coinciden')
+            }
+
+            return errores
+        },
+
+        erroresNombres: function(){
+            let errores = []
+            if(!this.$v.formulario.nombres.$dirty){
+                return errores
+            }
+
+            if(!this.$v.formulario.nombres.required){
+                errores.push('Ingresa tu nombre')
+            }
+
+            if(!this.$v.formulario.nombres.minLength){
+                errores.push('Ingresa un nombre de minimo 3 caracteres')
+            }
+
+            if(!this.$v.formulario.nombres.maxLength){
+                errores.push('Ingresa un nombre de maximo 20 caracteres')
+            }
+
+            if(!this.$v.formulario.nombres.nombreCompuesto){
+                errores.push('Ingresa un nombre valido')
+            }
+
+            return errores
+        },
+
+        erroresApellidos: function(){
+            let errores = []
+            if(!this.$v.formulario.apellidos.$dirty){
+                return errores
+            }
+
+            if(!this.$v.formulario.apellidos.required){
+                errores.push('Ingresa tus apellidos')
+            }
+
+            if(!this.$v.formulario.apellidos.minLength){
+                errores.push('Ingresa un apellido de minimo 3 caracteres')
+            }
+
+            if(!this.$v.formulario.apellidos.maxLength){
+                errores.push('Ingresa un apellido de maximo 20 caracteres')
+            }
+
+            if(!this.$v.formulario.apellidos.nombreCompuesto){
+                errores.push('Ingresa un apellido valido')
+            }
+
+            return errores
+        },
     },
     async mounted(){
         let id = this.$route.params.id
@@ -451,7 +685,71 @@ export default {
                 })
                 this.modalBoutique = false
             }
-        }
+        },
+        //Registro usuario
+        ...mapMutations([
+            'mostrarOcupado',
+            'ocultarOcupado',
+            'mostrarExito',
+            'mostrarError',
+            'mostrarAdvertencia'
+        ]),
+
+        ...mapMutations(
+            'sesion', ['actualizarUsuario']
+        ),
+
+        async guardarUsuario(uid){
+            let usuario = {
+                    uid,
+                    userName: 'Chaewon',
+                    nombres: this.formulario.nombres,
+                    apellidos: this.formulario.apellidos,
+                    //fechaNacimiento: new Date(this.fechaNacimiento),
+                    sexo: 'Femenino',
+                    descripcion: 'Descripcion',
+                    biografia: 'https://drama.fandom.com/es/wiki/Lee_Jin_Sol',
+                    fotoPerfil: 'https://hallyumusic.com/wp-content/uploads/2018/01/APRIL-Chaewon-The-Ruby-Cover.jpg',
+                    boutique: this.boutique.id
+                }
+
+                await db.collection('usuarios')
+                    .doc(usuario.uid)
+                    .set(usuario)
+
+                this.actualizarUsuario(usuario)
+                this.mostrarExito(this.saludo)
+        },
+
+        async registrarEmail(){
+            try{
+                this.mostrarOcupado({ titulo: 'Creando registro', mensaje: 'Estamos registrando tus datos' })
+
+                let credencial = await auth.createUserWithEmailAndPassword(this.formulario.email, this.formulario.password)
+                let uid = credencial.user.uid
+
+                await this.guardarUsuario(uid)
+
+                await auth.currentUser.sendEmailVerification()
+
+                this.$router.push({ name: 'verificacion-email' })
+            }
+            catch(error){
+                console.log(error)
+                switch (error.code) {
+                    case 'auth/email-already-in-use':
+                        this.mostrarAdvertencia('Ya se ha registrado esta direccion de email')
+                        break;
+
+                    default:
+                        this.mostrarError('Ocurrio un error registrando tu cuenta')
+                        break;
+                }
+            }
+            finally{
+                this.ocultarOcupado()
+            }
+        },
     }
 }
 </script>
